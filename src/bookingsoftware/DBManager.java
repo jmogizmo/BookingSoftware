fd8268f9da35abb170d4eb98a8d0264be04b1f99
Dorp <46204816+jojojo077@users.noreply.github.com>
2024-10-15 23:13:33 +1300

    database-implementation-v3.0

--- src/bookingsoftware/DBManager.java
+++ src/bookingsoftware/DBManager.java
@@ -11,24 +11,25 @@ import java.sql.ResultSet; // Use java.sql.ResultSet
  * @author dante
  */
 public class DBManager {
+
     private static final String USER_NAME = "root";
     private static final String PASSWORD = "root";
-    private static final String URL = "jdbc:derby:BookingSoftwareDB";
+    private static final String URL = "jdbc:derby:jdbc:derby:BookingSoftwareDB";
     public static Connection conn;
-    
+
     public static void main(String[] args) {
         DBManager DB = new DBManager();
         System.out.println(DB.getConnection());
     }
-    
+
     public DBManager() {
         establishConnection();
     }
-    
+
     public Connection getConnection() {
         return this.conn;
     }
-    
+
     public void establishConnection() {
         if (this.conn == null) {
             try {
@@ -39,7 +40,7 @@ public class DBManager {
             }
         }
     }
-    
+
     public void closeConnections() {
         if (conn != null) {
             try {
@@ -49,56 +50,53 @@ public class DBManager {
             }
         }
     }
-    
+
     public void appendToField(String table, String data) throws SQLException {
         // data e.g: " 421, 'user', 'us120', 908765476, '00000' "
         Statement statement = conn.createStatement();
-        
-            try {
-                statement.executeUpdate("INSERT INTO "+table+" VALUES ("+data+")");
-            } catch (SQLException ex) {
-                System.err.println(ex.getMessage());
-            }
+
+        try {
+            statement.executeUpdate("INSERT INTO " + table + " VALUES (" + data + ")");
+        } catch (SQLException ex) {
+            System.err.println(ex.getMessage());
+        }
         // You can implement your command execution logic here.
     }
-    
+
     public userInfo returnFieldFromUserInfo(String field, String target) throws SQLException {
         // Return generic type data from USERINFO
         // E.G: FIELD = studentID TARGET = 14
-        
+
         ResultSet rs = null;
         userInfo result = null;
-        String command = "SELECT * FROM USERINFO WHERE"+field+"="+target;
+        String command = "SELECT * FROM USERINFO WHERE" + field + "=" + target;
         Statement statement = conn.createStatement();
-        
+
         try {
             rs = statement.executeQuery(command);
             while (rs.next()) {
-                
+
                 int STUDENT_ID = rs.getInt("STUDENT_ID");
                 String FIRST_NAME = rs.getString("FIRST_NAME");
                 String EMAIL = rs.getString("EMAIL");
                 long PHONE = rs.getLong("PHONE");
                 String PASSWORD = rs.getString("PASSWORD");
-                
-                
+
                 try {
                     int intTarget = Integer.parseInt(target);
                     // WIP
                 } catch (NumberFormatException e) {
-                    
+
                 }
-                
-                
-                
+
             }
         } catch (SQLException ex) {
             System.err.println(ex.getMessage());
         }
-        
+
         rs.close();
         statement.close();
-        conn.close();   
-        return result; 
+        conn.close();
+        return result;
     }
 }
