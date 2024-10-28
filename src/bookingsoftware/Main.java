347f9d828263b4b63a09eb92932e5a783dffc1b0
Dorp <46204816+jojojo077@users.noreply.github.com>
2024-10-28 17:55:26 +1300

    conglomerate bug

--- src/bookingsoftware/Main.java
+++ src/bookingsoftware/Main.java
@@ -19,18 +19,31 @@ public class Main extends JFrame {
 
     public static void main(String[] args) {
 
-        if (DBManager.establishConnection() == 0) { // RUN PROGRAM IF CAN ESTABLISH CONNECTION TO DATABASE
+        if (DBManager.establishConnection() == 0) {
             SwingUtilities.invokeLater(() -> {
+
+                //load model
+                UserManager userManager = new UserManager();
+
+                //load all users from db on startup
+                try {
+                    
+                    userManager.loadUsers();
+
+                    
+                } catch (SQLException ex) {
+                    Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+                }
+
+                //load views
                 LoginRegisterView2 loginView = new LoginRegisterView2();
-                UserManager model = new UserManager();
-                try { model.loadUsers();
-                } catch (SQLException ex) { Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);}
                 MainMenuView2 menuView = new MainMenuView2();
                 UserDetailsView2 userDetailsView = new UserDetailsView2();
-                BookingInfo bookingInfo = new BookingInfo();
-                new LoginRegisterController(loginView, model, menuView, userDetailsView);
-                new MainMenuController(menuView, loginView, model);
-                new BookingController(menuView, bookingInfo);
+
+                //load controllers
+                new LoginRegisterController(loginView, userManager, menuView, userDetailsView);
+                new MainMenuController(menuView, loginView, userManager);
+                new BookingController(menuView, userManager);
                 loginView.setVisible(true);
             });
         } else {
